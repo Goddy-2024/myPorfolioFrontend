d6b7cda64aacd917bc4fc9c60a75aeadac5e7b53
GodswillOmondi <gaoajuoga@gmail.com>
2025-08-27 05:55:13 +0300

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -23,7 +23,7 @@ const About = () => {
           <div className="animate-slide-in">
             <div className="relative">
               <img 
-                src="/uploads/me.jpg" 
+                src="/uploads/goddy.jpg" 
                 alt="Godswill Portrait" 
                 className="w-full h-auto rounded-lg shadow-xl"
               />
